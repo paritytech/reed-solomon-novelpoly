5100fb939dc82c4457866a29d333ecf6df4fc4ca
Bernhard Schuster <bernhard@ahoi.io>
2021-03-02 15:12:00 +0100

    c version foo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use rs_ec_perf::*;
 
 fn main() {
+	// roundtrip(novel_poly_basis_cxx::encode, novel_poly_basis_cxx::reconstruct, &BYTES[..DATA_SHARDS * 2]);
 	roundtrip(novel_poly_basis::encode, novel_poly_basis::reconstruct, &BYTES[..DATA_SHARDS * 2]);
 	roundtrip(status_quo::encode, status_quo::reconstruct, &BYTES[..DATA_SHARDS * 2]);
 }
