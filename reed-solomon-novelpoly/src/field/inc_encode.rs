paritytech/reed-solomon-novelpoly

commit bd743c9178cafb90a0402009e8ae371fae264609

another small encoding optimisation (#31)
* another small encoding optimisation

improves performance by about 8%

Signed-off-by: alindima <alin@parity.io>

* fix

* fix clippy

---------

Signed-off-by: alindima <alin@parity.io>

reed-solomon-novelpoly/Cargo.toml
@@ -26,12 +26,12 @@ derive_more = { version = "0.99.0", default-features = false, features = [
   "add_assign",
   "add",
 ] }
-itertools = "0.11.0"
 # mock only
 reed-solomon-tester = { path = "../reed-solomon-tester", optional = true }
 rand = { version = "0.8.3", features = ["alloc", "small_rng"], optional = true }
 
 [dev-dependencies]
+itertools = "0.11"
 reed-solomon-tester = { path = "../reed-solomon-tester" }
 rand = { version = "0.8.3", features = ["alloc", "small_rng"] }
 assert_matches = "1.5.0"

reed-solomon-novelpoly/src/field/inc_encode.rs
@@ -25,12 +25,12 @@ pub fn encode_low_plain(data: &[Additive], k: usize, codeword: &mut [Additive], 
 	assert_eq!((n / k) * k, n);
 
 	// move the data to the codeword
-    codeword.copy_from_slice(data);
+	codeword.copy_from_slice(data);
 
 	// split after the first k
 	let (codeword_first_k, codeword_skip_first_k) = codeword.split_at_mut(k);
 
-    inverse_afft(codeword_first_k, k, 0);
+	inverse_afft(codeword_first_k, k, 0);
 
 	// dbg!(&codeword_first_k);
 	// the first codeword is now the basis for the remaining transforms
@@ -75,7 +75,7 @@ pub fn encode_low_faster8(data: &[Additive], k: usize, codeword: &mut [Additive]
 	let (codeword_first_k, codeword_skip_first_k) = codeword.split_at_mut(k);
 
 	assert!((k >> 1) >= Additive8x::LANE);
-    inverse_afft_faster8(codeword_first_k, k, 0);
+	inverse_afft_faster8(codeword_first_k, k, 0);
 
 
 	// the first codeword is now the basis for the remaining transforms
@@ -194,17 +194,16 @@ pub fn encode_sub_plain(bytes: &[u8], n: usize, k: usize) -> Result<Vec<Additive
 	assert!(is_power_of_2(upper_len));
 	assert!(upper_len >= bytes_len);
 
-	// tuple are only used here
-	use itertools::Itertools;
-
 	// pad the incoming bytes with trailing 0s
 	// so we get a buffer of size `N` in `GF` symbols
-	let zero_bytes_to_add = n * 2 - bytes_len;
-	let mut elm_data = Vec::with_capacity(n);
-	let zeros = std::iter::repeat(&0u8).take(zero_bytes_to_add);
-	for (first, second) in bytes.iter().chain(zeros).tuples() {
-		elm_data.push(Additive(Elt::from_be_bytes([*first, *second])));
-	}
+	let mut elm_data = vec![Additive(0); n];
+
+	for i in 0..(bytes_len / 2) {
+		elm_data[i] = Additive(Elt::from_be_bytes([
+			bytes.get(2 * i).copied().unwrap_or_default(),
+			bytes.get(2 * i + 1).copied().unwrap_or_default(),
+		]))
+ 	}
 
 	// update new data bytes with zero padded bytes
 	// `l` is now `GF(2^16)` symbols
@@ -243,17 +242,16 @@ pub fn encode_sub_faster8(bytes: &[u8], n: usize, k: usize) -> Result<Vec<Additi
 	assert!(is_power_of_2(upper_len));
 	assert!(upper_len >= bytes_len);
 
-    // tuples are only used here
-    use itertools::Itertools;
-
 	// pad the incoming bytes with trailing 0s
 	// so we get a buffer of size `N` in `GF` symbols
-	let zero_bytes_to_add = n * 2 - bytes_len;
-	let mut elm_data = Vec::with_capacity(n);
-	let zeros = std::iter::repeat(&0u8).take(zero_bytes_to_add);
-	for (first, second) in bytes.iter().chain(zeros).tuples() {
-		elm_data.push(Additive(Elt::from_be_bytes([*first, *second])));
-	}
+	let mut elm_data = vec![Additive(0); n];
+
+	for i in 0..(bytes_len / 2) {
+		elm_data[i] = Additive(Elt::from_be_bytes([
+			bytes.get(2 * i).map(|x| *x).unwrap_or_default(),
+			bytes.get(2 * i + 1).map(|x| *x).unwrap_or_default(),
+		]))
+ 	}
 
 	// update new data bytes with zero padded bytes
 	// `l` is now `GF(2^16)` symbols

.github/workflows/ci.yml
@@ -98,4 +98,4 @@ jobs:
       - uses: actions-rs/cargo@v1
         with:
           command: clippy
-          args: --workspace --all-features
+          args: --workspace --all-features -- -D warnings

reed-solomon-novelpoly/src/field/inc_afft.rs
@@ -507,7 +507,7 @@ pub mod test_utils {
 		let plain = plain.as_ref();
 		let faster8 = faster8.as_ref();
 
-		itertools::assert_equal(plain, faster8);
+		assert!(plain.eq(faster8));
 	}
 
 }

commit 4f771975ee33b3ba8c1c4d7329604e4fb1cc865d

add quickcheck tests and fix off-by-one bug introduced in #31

reed-solomon-novelpoly/Cargo.toml
@@ -35,6 +35,7 @@ itertools = "0.12"
 reed-solomon-tester = { path = "../reed-solomon-tester" }
 rand = { version = "0.8.3", features = ["alloc", "small_rng"] }
 assert_matches = "1.5.0"
+quickcheck = { version = "1.0.3", default-features = false }
 
 [features]
 default = []

reed-solomon-novelpoly/src/field/inc_encode.rs
@@ -190,7 +190,9 @@ pub fn encode_sub_plain(bytes: &[u8], n: usize, k: usize) -> Result<Vec<Additive
 	// so we get a buffer of size `N` in `GF` symbols
 	let mut elm_data = vec![Additive(0); n];
 
-	for i in 0..(bytes_len / 2) {
+	let tmp = if bytes_len % 2 == 0 { bytes_len / 2 } else { (bytes_len + 1) / 2 };
+
+	for i in 0..tmp {
 		elm_data[i] = Additive(Elt::from_be_bytes([
 			bytes.get(2 * i).copied().unwrap_or_default(),
 			bytes.get(2 * i + 1).copied().unwrap_or_default(),
@@ -241,7 +243,9 @@ pub fn encode_sub_faster8(bytes: &[u8], n: usize, k: usize) -> Result<Vec<Additi
 	// so we get a buffer of size `N` in `GF` symbols
 	let mut elm_data = vec![Additive(0); n];
 
-	for i in 0..(bytes_len / 2) {
+	let tmp = if bytes_len % 2 == 0 { bytes_len / 2 } else { (bytes_len + 1) / 2 };
+
+	for i in 0..tmp {
 		elm_data[i] = Additive(Elt::from_be_bytes([
 			bytes.get(2 * i).map(|x| *x).unwrap_or_default(),
 			bytes.get(2 * i + 1).map(|x| *x).unwrap_or_default(),

reed-solomon-novelpoly/src/novel_poly_basis/tests.rs
@@ -9,6 +9,7 @@ use rand::rngs::SmallRng;
 use rand::seq::index::IndexVec;
 use rand::thread_rng;
 use reed_solomon_tester::*;
+use quickcheck::{Arbitrary, Gen, QuickCheck};
 
 /// Generate a random index
 fn rand_gf_element() -> Additive {
@@ -462,4 +463,33 @@ fn shard_len_is_reasonable() {
 
 	// needs 3 bytes to fit, rounded up to next even number.
 	assert_eq!(rs.shard_len(19), 6);
+}
+
+#[derive(Clone, Debug)]
+struct ArbitraryData(Vec<u8>);
+
+impl Arbitrary for ArbitraryData {
+	fn arbitrary(g: &mut Gen) -> Self {
+		// Limit the len to 1 mib, otherwise the test will take forever
+		let len = u32::arbitrary(g).saturating_add(2) % (1024 * 1024);
+
+		let data: Vec<u8> = (0..len).map(|_| u8::arbitrary(g)).collect();
+
+		ArbitraryData(data)
+	}
+}
+
+#[test]
+fn round_trip_quickcheck() {
+	fn property(available_data: ArbitraryData, n_validators: u16) {
+		let n_validators = n_validators.saturating_add(2);
+		let wanted_k = (n_validators as usize - 1) / 3 + 1;
+		let rs = CodeParams::derive_parameters(n_validators as usize, wanted_k).unwrap().make_encoder();
+		let chunks = rs.encode::<WrappedShard>(&available_data.0).unwrap();
+		let mut res = rs.reconstruct(chunks.into_iter().take(wanted_k).map(|s| Some(s)).collect()).unwrap();
+		res.truncate(available_data.0.len());
+		assert_eq!(res, available_data.0);
+	}
+
+	QuickCheck::new().quickcheck(property as fn(ArbitraryData, u16))
 }